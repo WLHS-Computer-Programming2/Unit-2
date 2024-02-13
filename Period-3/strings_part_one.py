9db5aab9e48efa401319bfd3827514cee5b3d584
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2024-02-13 12:31:16 -0800

    Add files via upload

---
+++ Period-3/strings_part_one.py
@@ -0,0 +1,16 @@
+'''
+Name: Mr. Smith
+Date: 2/13/2024
+Class: Period 3
+Description: Covering f-strings, functions with default parameters,
+string indexing, string slicing, as well as if __name__
+'''
+
+
+def main():
+    print("Hello Mr. Smith")
+
+print("Hi")
+
+if __name__ == '__main__':
+    main()

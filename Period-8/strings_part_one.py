4a4b6f9f84f43b2398506d09372620f58cfea688
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-14 14:47:31 -0800

    class done

--- Period-8/strings_part_one.py
+++ Period-8/strings_part_one.py
@@ -18,14 +18,35 @@ def slicing_demo(string):
     #                    goes through end of string
     print(string[0:len(string):2]) # every other letter starting at M
     print(string[ : :2]) # every other letter starting at M
-    print(string[]) # get Smith using negative indices
+    print(string[-5::]) # get Smith using negative indices
+
+    # printing backward
+    print(string[-1:-5:-1]) # print Smith backwards
+    print(string[ : :-1]) # reverse entire string
+
+    # Python oddity
+    print(string[4:1000000]) # no out of bounds error here
+    #print(string[1000000]) # this will be an error b/c not slicing
+
+    # Practice in CodeHS with Sandwich Sandwiches
+    # in Indexing and 
+    # If You're Not First, You're Last
+    # and replace a letter, both parts, in Slicing
+
+def salutations(name:str,suffix:str,title:str="boring")->None:
+    print(f"Hello {name} the {title} {suffix}.")
+
+    # had to reorder parameters in order to use default argument
+    # for title
 
 def main():
+    salutations("Brandon","the first")
+
     # name = M  r  .     S  m  i  t  h
     #        0  1  2  3  4  5  6  7  8
     #       -9 -8 -7 -6 -5 -4 -3 -2 -1
-    name = "Mr. Smith"
-    slicing_demo(name)
+    #name = "Mr. Smith"
+    #slicing_demo(name)
 
 if __name__ == "__main__":
     main()

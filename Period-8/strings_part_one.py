b78e79195dd0f8140630abf43ec5baf4aadcbe50
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-14 13:55:45 -0800

    new folder

---
+++ Period-8/strings_part_one.py
@@ -0,0 +1,8 @@
+'''
+Name: Mr. Smith
+Date: 2/14/2024
+Class: Period 8
+Description: Covering f-strings, functions with default parameters,           f       
+string indexing, string slicing, as well as if __name__
+'''
+

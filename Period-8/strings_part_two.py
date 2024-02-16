41edea92f1069c7009a82e9ca775649f08488cca
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-16 14:05:19 -0800

    mid-lesson push

--- Period-8/strings_part_two.py
+++ Period-8/strings_part_two.py
@@ -1,10 +1,20 @@
-
-
-
-
 def main():
     cat_name = "Diamond"
     # cat_name[0] = "d" strings are immutable, cannot assign a character
+    # cat_name = "d"+cat_name[1:]
+    # print(cat_name)
+    list_of_numbers = "1323489734"
+    num_evens = 0
+    # non-pythonic way
+    for i in range(0,len(list_of_numbers),2):
+        print(list_of_numbers[i])
+    num_evens = 0
+    # pythonic way
+    for number in list_of_numbers:
+        if int(number) % 2 == 0:
+            num_evens += 1
+    print(num_evens)
+
 
 if __name__ == "__main__":
     main()

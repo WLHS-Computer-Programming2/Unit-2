c8475969a9fd8cfb49ac8cafdf74a3c2d7e70590
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-16 14:08:14 -0800

    pre-checkin push

--- Period-8/strings_part_two.py
+++ Period-8/strings_part_two.py
@@ -6,8 +6,10 @@ def main():
     list_of_numbers = "1323489734"
     num_evens = 0
     # non-pythonic way
-    for i in range(0,len(list_of_numbers),2):
-        print(list_of_numbers[i])
+    for i in range(len(list_of_numbers)):
+        if int(list_of_numbers[i]) % 2 == 0:
+            num_evens += 1
+    print(num_evens)
     num_evens = 0
     # pythonic way
     for number in list_of_numbers:
@@ -15,6 +17,14 @@ def main():
             num_evens += 1
     print(num_evens)
 
+    # printing every other item
+    for i in range(0,len(list_of_numbers),2):
+        print(num_evens)
+
+    # write a function called vowel_counter that will 
+    # go through an arbitrary string parameter and 
+    # count how many vowels (AEIOU or aeiou) are in it
+    # This count will be returned, not printed
 
 if __name__ == "__main__":
     main()

cfd6bb81b002e09938b056ed829444d3d62e8391
staatsd <staatsd@wlhs.wlwv.k12.or.us>
2025-04-02 09:20:09 -0700

    jyftul

--- Lesson7/main.py
+++ Lesson7/main.py
@@ -2,7 +2,10 @@ import Car
 import ElectricCar
 
 def main():
-    my_leaf = ElectricCar()
+    my_leaf = ElectricCar.ElectricCar("Nissan","Leaf",2024,60)
+    print(my_leaf.battery_level)
+    print(my_leaf.make)
+    
 
 if __name__ == '__main__':
     main()

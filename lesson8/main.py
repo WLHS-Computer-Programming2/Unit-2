cd5da71c77cb0f85464c65af8ef6471a781d4675
staatsd <staatsd@wlhs.wlwv.k12.or.us>
2025-04-09 08:33:10 -0700

    lub;

---
+++ lesson8/main.py
@@ -0,0 +1,9 @@
+from car import Car
+from electric_car import ElectricCar as EC
+def main():
+    my_leaf = EC("Nissan","Leaf",2024,60)
+    print(my_leaf.make)
+
+
+if __name__ == '__main__':
+    main()
